d019f80b44e200e8cbbecc391bf48e105b94e1d1
Slav <exemple@gmail.com>
2021-06-04 23:53:25 +0300

    add webpack

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="b">
+            Hello REact
+        </div>
+    )
+}
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: ["@babel/polyfill", "./src/index.jsx"],
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "[name].[hash].js"
+    },
+    devServer: {
+        port: 8080
+    },
+    module: {
+        rules: [
+            {
+            test: /\.scss$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                "css-loader", "sass-loader"
+            ],
+        },
+        {
+            test: /\.(jpg|jpeg|png|svg)/,
+            use: ['file-loader']
+        },
+        {
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env"]
+                }
+                
+            }
+        },
+        {
+            test: /\.jsx$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ["@babel/preset-env", "@babel/preset-react"]
+                }
+            }
+        }
+    ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "style.css"
+         }),
+        new HTMLWebpackPlugin({
+            filename: 'index.html',
+            template: "./src/index.html"
+        }),
+        new CleanWebpackPlugin(),
+    ]
+}
