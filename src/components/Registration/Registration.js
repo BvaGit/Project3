1843a38f8fa464be8c4cc17ea9f2d9ccefcaf402
Maryna Hrushevska <mia.grushevska@gmail.com>
2021-06-15 13:30:02 +0300

    reg page func instead of class

--- src/components/Registration/Registration.js
+++ src/components/Registration/Registration.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './Registration.scss';
 import Input from '../Input';
 import Button from '../Button';
@@ -6,32 +6,59 @@ import Logo from '../Logo';
 import Title from '../Title';
 import { NavLink } from 'react-router-dom';
 
-class Registration extends Component {
-    render(){
-        return(
-            <div className="req__holder">
-                <div className="req">
-                    <div className="req__info">
-                        <Logo src="../../public/assets/images/logo.png" alt="logo"/>
-                        <Title textBig="SIGN UP" textSmall="YOUR ACCOUNT"/>
-                    </div>
-                    <div className="req__main">
-                        <div className="req__container">
-                            <Input label="Login" type="text" placeholder="Enter your login"/>
-                            <Input label="Password" type="password" placeholder="Enter your password"/>
-                            <Input label="Repeat password" type="password" placeholder="Confirm your password"/>
-                            <div className="req__buttons">
-                                <Button type="button" text="SIGN IN"/>
-                            </div>
-                            <p className="req__link">Already have an account?</p>
-                            <NavLink to='/auth' className="req__link">
-                                Sign in
-                            </NavLink>
+const Registration = () => {
+    return(
+        <div className="req__holder">
+            <div className="req">
+                <div className="req__info">
+                    <Logo src="../../public/assets/images/logo.png" alt="logo"/>
+                    <Title textBig="SIGN UP" textSmall="YOUR ACCOUNT"/>
+                </div>
+                <div className="req__main">
+                    <div className="req__container">
+                        <Input label="Login" type="text" placeholder="Enter your login"/>
+                        <Input label="Password" type="password" placeholder="Enter your password"/>
+                        <Input label="Repeat password" type="password" placeholder="Confirm your password"/>
+                        <div className="req__buttons">
+                            <Button type="button" text="SIGN IN"/>
                         </div>
+                        <p className="req__link">Already have an account?</p>
+                        <NavLink to='/auth' className="req__link">
+                            Sign in
+                        </NavLink>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+// class Registration extends Component {
+//     render(){
+//         return(
+//             <div className="req__holder">
+//                 <div className="req">
+//                     <div className="req__info">
+//                         <Logo src="../../public/assets/images/logo.png" alt="logo"/>
+//                         <Title textBig="SIGN UP" textSmall="YOUR ACCOUNT"/>
+//                     </div>
+//                     <div className="req__main">
+//                         <div className="req__container">
+//                             <Input label="Login" type="text" placeholder="Enter your login"/>
+//                             <Input label="Password" type="password" placeholder="Enter your password"/>
+//                             <Input label="Repeat password" type="password" placeholder="Confirm your password"/>
+//                             <div className="req__buttons">
+//                                 <Button type="button" text="SIGN IN"/>
+//                             </div>
+//                             <p className="req__link">Already have an account?</p>
+//                             <NavLink to='/auth' className="req__link">
+//                                 Sign in
+//                             </NavLink>
+//                         </div>
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 export default Registration;
