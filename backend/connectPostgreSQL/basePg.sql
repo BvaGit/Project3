b9a85c6d8b107026069c23d0e2e06663d3b97347
Slav <exemple@gmail.com>
2021-06-17 19:40:36 +0300

    add myaccount and update loginPassword

--- backend/connectPostgreSQL/basePg.sql
+++ backend/connectPostgreSQL/basePg.sql
@@ -6,3 +6,17 @@ CREATE TABLE users (
     password VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE myaccount(
+    id BIGSERIAL PRIMARY KEY,
+    firstname VARCHAR(100),
+    lastname VARCHAR(100),
+    age INTEGER,
+    city VARCHAR(100),
+    company VARCHAR(100),
+    hobbi VARCHAR(250),
+    avatar VARCHAR(250),
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+
